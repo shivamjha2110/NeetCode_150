481bc8074932a8ecedcafe16d8ea361d82fc7a2c
Shivam jha <shivamjha21108@gmail.com>
2023-10-20 20:13:35 +0530

    Time: 3 ms (40.79%), Space: 6.9 MB (30.32%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,41 +1,24 @@
-/*
-
-    Time Complexity : O(3^N × 4^M) where N is the number of digits which have 3 letters( ex: 2,3,4) assigned to
-    it and M is the number of digits which has 4 letters(ex: 7,9) assigned to it.
-
-    Space Complexity : O(3^N × 4^M) where N is the number of digits which have 3 letters( ex: 2,3,4) assigned to
-    it and M is the number of digits which has 4 letters(ex: 7,9) assigned to it.
-
-    Solved using String + Backtracking + Hash Table. Recursive Approach.
-
-*/
-
-
-/***************************************** Approach 1 *****************************************/
-
 class Solution {
-private:
-    void letterCombinations(string digits, vector<string>& output, string &temp, vector<string>& pad, int index){
-        if(index == digits.size()){
-            output.push_back(temp);
-            return;
-        }
-        string value = pad[digits[index]-'0'];
-        for(int i=0; i<value.size(); i++){
-            temp.push_back(value[i]);
-            letterCombinations(digits, output, temp, pad, index+1);
-            temp.pop_back();
-        }
-    }
 public:
+    // solved using string + Hash Table (Iterative Approach)
+    
     vector<string> letterCombinations(string digits) {
-        if(digits.empty()){
+         if(digits.empty()){
             return {};
         }
         vector<string> pad = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         vector<string> output;
-        string temp;
-        letterCombinations(digits, output, temp, pad, 0);
+        output.push_back("");
+        for(auto key : digits){
+            vector<string> temp;
+            for(auto candidate : pad[key-'0']){
+                for(auto c : output){
+                    temp.push_back(c + candidate);
+                }
+            }
+            output.clear();
+            output = temp;
+        }
         return output;
     }
 };
