b89d9bb947397a66241cde013fd51eeedba9ada1
Shivam jha <shivamjha21108@gmail.com>
2023-10-23 21:43:08 +0530

    Time: 20 ms (47.64%), Space: 14.5 MB (15.37%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,30 +10,41 @@
  */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists)
-    {
-        //min heap based on Node value, the minimum value will be at the top in priority queue
-        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> pq;
-        for (ListNode* listHead : lists) 
-        {
-            if (listHead != NULL) pq.push({listHead->val, listHead});
-        }
-        //=================================================================================
-        ListNode* newHead = new ListNode(-1); //dummy node for the new Linked List
-        ListNode* newTail = newHead;
-        while(!pq.empty())
-        {
-            int minVal = pq.top().first;
-            ListNode* minNode = pq.top().second;
-            pq.pop();
-            if (minNode->next != NULL) 
-                pq.push({minNode->next->val, minNode->next});
-            
-            newTail->next = minNode;  //append the min Node to the new Linked List
-            newTail = minNode; //make the minNode as the new tail of new Linked List
-        }
-        //=======================================================================================
-        return newHead->next;
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int k = lists.size(); // taking size of the list
+        if(k == 0) // if size is zero
+            return NULL; // simply return NULL
         
+        // making a vector pair where first contain value and second contain node
+        vector<pair<int, ListNode*>> arr; 
+        
+        for(int i = 0; i < k; i++) // traverse all over the list
+        {
+            ListNode* curr_list = lists[i]; // extracting current linked list
+            
+            while(curr_list != NULL) // while current linked list is NOT NULL
+            {
+                arr.push_back({curr_list -> val, curr_list}); // push into vector
+                curr_list = curr_list -> next;
+            }
+        }
+        
+        // this does not gurantee that k is zero, 
+        // suppose an array like this [[],[],[],],here k = 3 and size of array is 0
+        if(arr.size() == 0) // if their is no element i.e zero element
+            return NULL;
+        
+        sort(arr.begin(), arr.end()); // sort the vector on the basis of values
+        
+        // start making links b/w the elements of vector
+        for(int i = 0; i < arr.size() - 1; i++)
+        {
+            arr[i].second -> next = arr[i + 1].second;
+        }
+        
+        // in the next of last node put NULL
+        arr[arr.size() - 1].second -> next = NULL;
+        
+        return arr[0].second; // return first node of the vector
     }
 };
