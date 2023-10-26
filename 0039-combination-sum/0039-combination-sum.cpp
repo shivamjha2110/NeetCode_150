770b869f2a4f545cb6090104b224f01a4ddca417
Shivam jha <shivamjha21108@gmail.com>
2023-10-26 20:08:34 +0530

    Time: 4 ms (74.91%), Space: 11 MB (69.01%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,43 +1,38 @@
+// Solution 01
+// We can take an element multiple times and add them to get the target value.
+// We’re using the help function where the base case is if the index reaches the last of our candidates array & the target became 0 then we’ll push temp to ans vector.
+// Now there could be 2 possible cases:
+// We’ll include the current element to our temp variable:
+// Check the current index value if it’s less than target then we can push it to temp and consider this as a part of our ans vector. Reduce the value from target.
+// after that we’ll pop the element from temp, as when it return from this call means that value didn’t give us expected ans.
+// We won’t include the current element and move to the next element.
+// Time complexity: O(2^k), where k is the sum of target/candidate[i] from i = 0 to size of candidate - 1.
+
+// Code:
+//Solution 01:
 class Solution {
 public:
-
-    void Sum(vector<int>& candidates, int target, vector<vector<int> >& res, vector<int>& r, int i)
-    {
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
         
-        if(target == 0)
-        {
-            // if we get exact answer
-            res.push_back(r);
+        help(0, candidates, target, ans, temp);
+        return ans;
+    }
+    
+    void help(int index, vector<int>& candidates, int target, vector<vector<int>> &ans, vector<int> &temp){
+        if(index == candidates.size()){
+            if(target == 0){
+                ans.push_back(temp);
+            }
             return;
         }
-        
-        while(i <  candidates.size() && target - candidates[i] >= 0)
-        {
-            // Till every element in the array starting
-            // from i which can contribute to the target
-            r.push_back(candidates[i]);// add them to vector
             
-            // recur for next numbers
-            Sum(candidates,target - candidates[i],res,r,i);
-            ++i;
-            
-            // Remove number from vector (backtracking)
-            r.pop_back();
+        if(candidates[index] <= target){
+            temp.push_back(candidates[index]);
+            help(index, candidates, target-candidates[index], ans, temp);
+            temp.pop_back();
         }
-}
-    
-     
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end()); // sort candidates array
-        
-        // remove duplicates
-        candidates.erase(unique(candidates.begin(),candidates.end()),candidates.end());
-        
-        vector<int> r;
-        vector<vector<int> > res;
-        
-        Sum(candidates,target,res,r,0);
-        
-        return res;
+        help(index+1, candidates, target, ans, temp);
     }
 };
