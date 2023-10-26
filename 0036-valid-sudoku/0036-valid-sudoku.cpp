0c148a321203d7b4829bb6b5453a33682ca22044
Shivam jha <shivamjha21108@gmail.com>
2023-10-26 11:49:57 +0530

    Time: 11 ms (92.64%), Space: 20.6 MB (27.20%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        // Three Cases so 3 condtions.
-        int rowcase[9][9] ={0};
-        int colcase[9][9] = {0};
-        int gridcase[9][9] = {0};
-
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-         // if it is number.
-                if(board[i][j] != '.'){
-                    int number = board[i][j] - '0';
-
-
-                    int k = i/3 * 3 + j/3 ;
-                    // k denotes grid number
-
-                    // check for the 3 case.
-     if(rowcase[i][number-1]++ || colcase[j][number-1]++ || gridcase[k][number-1]++)
+          
+        vector<set<int>> row(9),col(9),box(9);
+        
+        for(int i=0;i< 9;i++)
+        {
+            for(int j = 0;j < 9;j++)
+            {
+                if(board[i][j] != '.')
+                {
+                    int num = board[i][j] - '0';
+                    
+                    if(row[i].count(num) || col[j].count(num) || box[(i/3)*3 + (j/3)].count(num))
+                    {
                         return false;
-
+                    }
+                    
+                    row[i].insert(num);
+                    col[j].insert(num);
+                    box[(i/3)*3 + (j/3)].insert(num);
+            
                 }
             }
         }
+        
         return true;
     }
 };
