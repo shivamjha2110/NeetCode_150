f8844c617e96c30bf66a4cc101a8176323e49f2c
Shivam jha <shivamjha21108@gmail.com>
2023-11-16 18:28:19 +0530

    Time: 3 ms (70.63%), Space: 9.7 MB (91.09%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-		// running a forloop till size of matrix
-        for(int i  = 0; i<matrix.size(); ++i)
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+		// if matrix have 0 rows or 0 colums
+        if(n == 0 || m == 0)
+            return false;
+        
+		// treating matrix as array just taking care of endices
+		// [0..n*m]
+        int start = 0, end = m*n - 1;
+        
+        while(start <= end)
         {
-		// nested for for ietrating each row element
-            for(int j = 0; j<matrix[0].size(); ++j)
-            {
-				// if found return true
-                if(matrix[i][j] == target)
-                    return true;
-            }
+            int mid = start + (end - start) / 2;
+			// a[x] : matrix[x / m][x % m] formulae
+            int ind = matrix[mid/m][mid%m];
+            if (target == ind)
+                return true;
+			// left half
+            else if(target < ind)
+                end = mid - 1;
+            else
+			// right half
+                start = mid + 1;       
         }
-		// after traversal if not found
-		// return false
         return false;
     }
 	// for github repository link go to my profile.
